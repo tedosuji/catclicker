dc3d670224934887cf023d784f742af6698c47a0
tedosuji <potatostudent@gmail.com>
2020-09-12 14:21:04 -0700

    refactored for global scene

--- src/Main.gd
+++ src/Main.gd
@@ -1,34 +1,29 @@
 extends Node2D
 
-var cookies = 0
-var cookieUpgrade = 10
-var cookieUpgradeScaling = 1
-var cookiesPerSecond = 1
-
 onready var silvercoin = $bg/catcoin_0/silvercoin
 
 func _on_catbutton_pressed():
-	cookies += 1
-	silvercoin.text = str(cookies)
+	Global.cookies += 1
+	silvercoin.text = str(Global.cookies)
 
 #this is basic cookie timer increase every second
 func _on_Timer_timeout():
-	cookies += cookiesPerSecond
-	silvercoin.text = str(cookies)
+	Global.cookies += Global.cookiesPerSecond
+	silvercoin.text = str(Global.cookies)
 
-#this function should subtract silver cookies
+#this function should subtract silver Global.cookies
 func _on_upgradebutton_pressed():
-	var skip = _enoughCookieCheck(cookieUpgrade)
+	var skip = _enoughCookieCheck(Global.cookieUpgrade)
 	if skip == true:
-		cookies -= cookieUpgrade
-		silvercoin.text = str(cookies)
-		cookieUpgrade = cookieUpgrade*cookieUpgradeScaling
-		cookieUpgradeScaling += 1
+		Global.cookies -= Global.cookieUpgrade
+		silvercoin.text = str(Global.cookies)
+		Global.cookieUpgrade = Global.cookieUpgrade*Global.cookieUpgradeScaling
+		Global.cookieUpgradeScaling += 1
 
-#this function checks if there is enough cookies for transactions
+#this function checks if there is enough Global.cookies for transactions
 #if there isnt, return false
 func _enoughCookieCheck(check):
-	if cookies - check < 0:
+	if Global.cookies - check < 0:
 		return false
 	else:
 		return true
