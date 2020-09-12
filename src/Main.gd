59b7705c9d9bbbda17bbeba999a9c472fc428b5e
tedosuji <potatostudent@gmail.com>
2020-09-12 14:04:49 -0700

    cookie multiplier, cookie check, git cheatsheet

--- src/Main.gd
+++ src/Main.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
 var cookies = 0
+var cookieUpgrade = 10
+var cookieUpgradeScaling = 1
 var cookiesPerSecond = 1
 
 onready var silvercoin = $bg/catcoin_0/silvercoin
@@ -9,7 +11,24 @@ func _on_catbutton_pressed():
 	cookies += 1
 	silvercoin.text = str(cookies)
 
-
+#this is basic cookie timer increase every second
 func _on_Timer_timeout():
 	cookies += cookiesPerSecond
 	silvercoin.text = str(cookies)
+
+#this function should subtract silver cookies
+func _on_upgradebutton_pressed():
+	var skip = _enoughCookieCheck(cookieUpgrade)
+	if skip == true:
+		cookies -= cookieUpgrade
+		silvercoin.text = str(cookies)
+		cookieUpgrade = cookieUpgrade*cookieUpgradeScaling
+		cookieUpgradeScaling += 1
+
+#this function checks if there is enough cookies for transactions
+#if there isnt, return false
+func _enoughCookieCheck(check):
+	if cookies - check < 0:
+		return false
+	else:
+		return true
