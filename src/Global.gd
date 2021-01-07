3d0129fbbdbdf5d73caebab570179188ccc1523d
tedosuji <potatostudent@gmail.com>
2021-01-07 14:28:22 -0800

    changed cookies away to epoch

--- src/Global.gd
+++ src/Global.gd
@@ -38,7 +38,7 @@ func _loadCurrentSave():
 	
 #these functions stand to calculate the diff of cookies while not playing
 func _get_timestamp():
-	var time = OS.get_time();
+	var time = OS.get_unix_time();
 	var date = OS.get_date();
 	Global.stoppedTime = time
 	Global.stoppedDate = date
@@ -47,34 +47,11 @@ func _get_timestamp():
 func _calc_cookies_while_quit(savetime, savedate):
 	#get time to check against
 	var cookies_to_add = 0
-	var checksecs = 0
-	var savesecs = 0
-	var yeardiff = 0
-	var monthdiff = 0
-	var daydiff = 0
-	var checkTime = OS.get_time()
+	var checkTime = OS.get_unix_time();
 	var checkDate = OS.get_date()
 	
-	#convert all times to some form of seconds, calculate diff
-	if checkDate["day"] != savedate["day"]:
-		daydiff = (checkDate["day"] - savedate["day"])
-		if checkDate["month"] != savedate["month"]:
-			monthdiff = (checkDate["month"] - savedate["month"])
-			if checkDate["year"] != savedate["year"]:
-				yeardiff = (checkDate["year"] - savedate["year"])
-	else:
-		#start time of game or the "check time"
-		checksecs += checkTime["hour"]*3600
-		checksecs += checkTime["minute"]*60
-		checksecs += checkTime["second"]
-		
-		#saved variable of quit time passed to function
-		savesecs += savetime["hour"]*3600
-		savesecs += savetime["minute"]*60
-		savesecs += savetime["second"]
-	
 	#diff of seconds * cookiesPerSecond to credit silver coins
-	cookies_to_add = (checksecs - savesecs) * Global.cookiesPerSecond
+	cookies_to_add = (checkTime - savetime) * Global.cookiesPerSecond
 	
 	#return used to add in the main function
 	return cookies_to_add
